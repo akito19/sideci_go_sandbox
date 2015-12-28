1fc9d43ec64e0a2e2a16d886ff62ce37808aeab0
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-12-28 15:36:06 +0900

    Add dirty codes

---
+++ dirty.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	var hoge = "hogehoge"
+	var _ = hoge
+	ioutil.ReadFile("dirty.go")
+	fmt.Printf("%d\n")
+}
+
+func Foo(arg string) int {
+	return 1
+	return 1
+}
+
+func ComplexFunc(r io.Reader) error {
+	b, err := ioutil.ReadAll(r)
+	b, err = ioutil.ReadAll(r)
+	r.Read([]byte{})
+	if err != nil {
+		return err
+	}
+	a := 0
+	c := make(chan string, 0)
+	for a == 1 {
+		select {
+		case <-c:
+		default:
+			switch a {
+			case 1:
+				fmt.Println("hoge")
+			case 0:
+				fmt.Printf("hoge\n")
+			default:
+				os.Stdout.Write(b)
+			case 2:
+				if a == 4 {
+					if len(b) != 0 {
+						panic("panic!")
+					}
+				}
+				return nil
+			}
+
+		}
+	}
+
+	return nil
+}
+
+func ComplexFunc2(r io.Reader) error {
+	b, err := ioutil.ReadAll(r)
+	b, err = ioutil.ReadAll(r)
+	r.Read([]byte{})
+	if err != nil {
+		return err
+	}
+	a := 0
+	c := make(chan string, 0)
+	for a == 1 {
+		select {
+		case <-c:
+		default:
+			switch a {
+			case 1:
+				fmt.Println("hoge")
+			case 0:
+				fmt.Printf("hoge\n")
+			default:
+				os.Stdout.Write(b)
+			case 2:
+				if a == 4 {
+					if len(b) != 0 {
+						panic("panic!")
+					}
+				}
+				return nil
+			}
+
+		}
+	}
+	return nil
+}
+
+type BrokenStruct struct {
+	hoge string `aabbcc`
+}
+
+// dirty MarshalJSON
+func (b *BrokenStruct) MarshalJSON() int {
+	return 0
+}
+func (s *BrokenStruct) Hoge() {
+	return
+}
