322ba847dcaa6c732c7b8309f5b666c731ecbd1f
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-01-10 16:41:41 +0900

    Add more dependency

--- depend-lib.go
+++ depend-lib.go
@@ -3,10 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/actcat/sideci_go_sandbox/lib"
 	"github.com/naoina/denco"
 )
 
 func HttpHandler(w http.ResponseWriter, r *http.Request, p denco.Params) {
 	foo := p.Get("foo")
 	w.Write([]byte(foo))
+}
+
+func UseLibFoo() {
+	libfoo.Foo()
 }
