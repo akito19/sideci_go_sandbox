4cfe8554d939c7de2638f28b29ddea4c1814630d
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-01-13 12:17:26 +0900

    add dependency

--- depend-lib.go
+++ depend-lib.go
@@ -4,7 +4,11 @@ import (
 	"net/http"
 
 	"github.com/actcat/sideci_go_sandbox/lib"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/naoina/denco"
+	_ "github.com/naoina/kocha"
+	_ "github.com/revel/revel"
+	_ "github.com/zenazn/goji"
 )
 
 func HttpHandler(w http.ResponseWriter, r *http.Request, p denco.Params) {
