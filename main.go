eec74e2109d582073660ab223eebb930751cff88
Masataka Kuwabara <p.ck.t22@gmail.com>
2016-12-12 14:54:36 +0900

    Depends private repo

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
+	"github.com/sideci/go_private_library"
 )
 
 func main() {
 	e := echo.New()
+	fmt.Println(go_private_library.Fibo(10))
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
